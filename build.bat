096c0805a13ef15c48d715ef0b84b66bf7563ee7
frialey63 <paul.parlett@gmail.com>
2025-05-13 22:58:38 +0100

    changed tag for the Docker image

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ rem build.bat
 
 mvm -Pproduction clean package
 
-docker build -t pjp/sami-lingo-docker .
+docker build -t pjp/sami-lingo .
 
-docker run -p 8080:8080 pjp/sami-lingo-docker
+docker run -p 8080:8080 pjp/sami-lingo

d6e1318086b668d188486e012115228c409f5593
frialey63 <paul.parlett@gmail.com>
2025-05-15 12:40:48 +0100

    updated build script

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 rem build.bat
 
-mvm -Pproduction clean package
+#mvn -Pproduction clean package
 
 docker build -t pjp/sami-lingo .
 
-docker run -p 8080:8080 pjp/sami-lingo
+docker save -o sami-lingo.tar pjp/sami-lingo:latest
+scp .\sami-lingo.tar paul@192.168.0.40:/tmp
+#docker load -i /tmp/sami-lingo.tar
+
+#docker run -p 8080:8080 pjp/sami-lingo

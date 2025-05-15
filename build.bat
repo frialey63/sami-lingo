99e85816ee218659ce63cd3351108bedd705475c
frialey63 <paul.parlett@gmail.com>
2025-05-15 12:59:08 +0100

    updated build file

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem build.bat
 docker build -t pjp/sami-lingo .
 
 docker save -o sami-lingo.tar pjp/sami-lingo:latest
-scp .\sami-lingo.tar paul@192.168.0.40:/tmp
+#scp .\sami-lingo.tar paul@192.168.0.40:/tmp
 #docker load -i /tmp/sami-lingo.tar
 
-#docker run -p 8080:8080 pjp/sami-lingo
+#docker run -p 8090:8080 pjp/sami-lingo

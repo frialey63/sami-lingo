7cefaa5d2d27bc91ab38a3b82e87289a4975d6cd
frialey63 <paul.parlett@gmail.com>
2025-05-17 18:23:50 +0100

    use Windows rem to comment-out lines in batch

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 rem build.bat
 
-#mvn -Pproduction clean package
+rem mvn -Pproduction clean package
 
 docker build -t pjp/sami-lingo .
 
 docker save -o sami-lingo.tar pjp/sami-lingo:latest
-#scp .\sami-lingo.tar paul@192.168.0.40:/tmp
-#docker load -i /tmp/sami-lingo.tar
+rem scp .\sami-lingo.tar paul@192.168.0.40:/tmp
+rem docker load -i /tmp/sami-lingo.tar
 
-#docker run -p 8090:8080 pjp/sami-lingo
+rem docker run -p 8090:8080 pjp/sami-lingo
